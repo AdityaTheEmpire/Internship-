33372378ce17c9e9b4169f631df236dc1533de61
AdityaTheEmpire <135931436+AdityaTheEmpire@users.noreply.github.com>
2024-03-16 00:04:41 +0530

    Add files via upload

---
+++ Day2/javabasics.js
@@ -0,0 +1,6 @@
+food = 20
+tipprecentage = 0.2
+console.log("food*tipprecentage: ",food*tipprecentage)
+food = prompt("Enter the Bill")
+tipprecentage = 0.2
+alert("food*tipprecentage: " , food*tipprecentage)
